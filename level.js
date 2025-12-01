ccff3d31ab5c261067e78e6f2c46fb72d1b748c7
anmvillarica <b2029anmvillarica@pshs.edu.ph>
2025-12-01 13:47:34 +0800

    Create the base code for the game

--- level.js
+++ level.js
@@ -1,14 +1,24 @@
 const LEVEL = [1, 2, 3];
+const LEVEL1_STR = ["transform:", "255,0,0", "}"]
+const LEVEL2_STR = [ ]
 
 var sbt = document.getElementById("submit_btn");
 
-sbt.addEventListener("click", checkInput());
-
 function checkInput(){
-    var inp1 = document.getElementById("blank1");
-    
-    if (inp1 == "transform:")
+    var incorrect = [];
+
+    for (let i = 0; i < 3; i++)
     {
-        alert("Ang galing mo!");
+        var var_name = "inp" + (i + 1);
+        var id = "blank" + (i + 1);
+        var var_name = document.getElementById(id).value;
+
+        if (var_name != LEVEL1_STR[i])
+            incorrect.push(i);
     }
+
+    for (let i = 0; i < incorrect.length; i++)
+        alert(incorrect[i]);
+    if (incorrect.length == 0)
+        alert("Ang galing mo!");
 }
