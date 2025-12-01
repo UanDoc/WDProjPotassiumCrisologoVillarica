7f22e8ff93ea928840a168e43d02011859c37af9
anmvillarica <b2029anmvillarica@pshs.edu.ph>
2025-12-01 19:32:49 +0800

    Edit game code

---
+++ javascript/level.js
@@ -0,0 +1,39 @@
+const LEVEL1_STR = ["transform:", "255,0,0", "}"]
+const LEVEL2_STR = ["document.getElementbyId", "i = 0", "break"]
+const LEVEL3_STR = ["font-family:", "align-items:", "flexbox"]
+var levelSelector = 0; 
+
+var sbt = document.getElementById("submit_btn");
+
+function checkInput(){
+    var incorrect = [];
+
+    for (let i = 0; i < 3; i++)
+    {
+        var var_name = "inp" + (i + 1);
+        var id = "blank" + (i + 1);
+        var var_name = document.getElementById(id).value;
+
+        switch(levelSelector)
+        {
+            case 1: 
+                if (var_name != LEVEL1_STR[i])
+                    incorrect.push(i);
+                break; 
+            case 2: 
+                if (var_name != LEVEL2_STR[i])
+                    incorrect.push(i);
+                break; 
+            case 3: 
+                if (var_name != LEVEL3_STR[i])
+                    incorrect.push(i);
+                break; 
+        }
+        
+    }
+
+    for (let i = 0; i < incorrect.length; i++)
+        alert(incorrect[i]);
+    if (incorrect.length == 0)
+        alert("Ang galing mo!");
+}
